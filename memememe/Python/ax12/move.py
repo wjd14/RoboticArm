368c7360b0707054c64d27e2b5be9eb7c354ecdd
wjd14 <will@ayerdoyles.org>
2016-12-19 00:07:19 +0000

    possibly useless move file

--- memememe/Python/ax12/move.py
+++ memememe/Python/ax12/move.py
@@ -2,3 +2,13 @@ import sys;
 import math;
 import time;
 from ax12 import Ax12;
+
+
+class Mover:
+    self = Ax12();
+
+    def move(self, number, ticks, speed):
+        self.moveSpeed(number, ticks, speed);
+        return;
+
+    def read(self, number)
